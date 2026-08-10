9dc4bb5422e5568837aa33a0fddadd1bda16d51a
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-08-10 16:14:30 +0600

    added 9 no

---
+++ 05_Function_&_Modular_Programming/01_Function_Basics/No_08.py
@@ -0,0 +1,4 @@
+def area(num):
+    print(num * num)
+
+area(int(input("Insert the value: ")))
---
+++ 05_Function_&_Modular_Programming/01_Function_Basics/No_09.py
@@ -0,0 +1,8 @@
+def numbers(num1, num2):
+    for i in range(num1, num2):
+        print(i)
+
+# numbers(int(split(input("Insert the values: ")," ")))
+
+numbers(int(input("Insert first one: ")), int(input("Insert second one: ")))
+
