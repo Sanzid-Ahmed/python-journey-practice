aa692ab805a834b3d5574afbc9a6fe33c3342a14
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-08-10 15:48:56 +0600

    added 3no solution

---
+++ 05_Function_&_Modular_Programming/01_Function_Basics/No_03.py
@@ -0,0 +1,9 @@
+def about_you(name, age, university):
+    print("Hello ", name)
+    print("your age is: ", age)
+    print("Your university is: ", university)
+
+
+about_you(input("What is your name?\n"), input("What is your age?\n"), input("What is your university name?\n"))
+
+
