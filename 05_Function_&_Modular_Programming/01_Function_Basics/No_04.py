40cba745ab113a233d151a172be07e4e7748ac82
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-08-10 15:52:26 +0600

    added 4 no solution

---
+++ 05_Function_&_Modular_Programming/01_Function_Basics/No_04.py
@@ -0,0 +1,12 @@
+def numbers():
+    for i in range(10):
+        print(i)
+def numbers_1():
+    for i in range(1, 10):
+        print(i)
+
+print("Numbers from 0 to 10: ")
+numbers()
+print("Numbers from 1 to 10: ")
+numbers_1()
+
