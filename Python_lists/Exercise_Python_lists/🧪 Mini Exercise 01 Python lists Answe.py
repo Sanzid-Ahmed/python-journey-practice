811d0bff34c192992432d1b9cd6b4149bce4a0fc
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-09-21 15:00:35 +0600

    added Mini exercide 01

---
+++ Python_lists/Exercise_Python_lists/🧪 Mini Exercise 01 Python lists Answe.py
@@ -0,0 +1,41 @@
+"""
+
+numbers = [10, 20, 30, 40, 50, 60, 70] 
+
+1. print(numbers[3])
+ans: 40
+
+2. print(numbers[-2])
+ans: 60
+
+3. print(numbers[1:5])
+ans: [20, 30, 40, 50]
+
+4. print(numbers[:4]) 
+ans: [10, 20, 30, 40]
+
+5. print(numbers[3:]) 
+ans: [40, 50, 60, 70]
+
+6. print(numbers[::2])
+ans: [10, 40, 70] X
+correct ans: [10, 30, 50, 70]
+
+7. print(numbers[::-1])
+ans: [70, 60, 50, 40, 30, 20, 10]
+
+"""
+
+
+# verify answer: 
+
+numbers = [10, 20, 30, 40, 50, 60, 70] 
+
+
+print(numbers[3]) 
+print(numbers[-2]) 
+print(numbers[1:5]) 
+print(numbers[:4]) 
+print(numbers[3:]) 
+print(numbers[::2]) 
+print(numbers[::-1]) 
