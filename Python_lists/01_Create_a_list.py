dab1f537d94bf131e37b6a9cbdaf3f7c6bcf7455
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-09-21 14:09:25 +0600

    added first theory concept of python list

---
+++ Python_lists/01_Create_a_list.py
@@ -0,0 +1,17 @@
+numbers = [10, 20, 30,  40,  50]
+
+names = ["Sanzid", "Rahim", "Karim"]
+
+mixed = [10,"Python", 3.90, True]
+
+item = []
+
+
+
+# check the types: 
+
+print(type(numbers))
+print(type(names))
+print(type(mixed))
+print(type(item))
+
