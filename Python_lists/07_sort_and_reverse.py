447e96548172c214f170f2a19ae5e1f479f14ac8
Sanzid Ahmed <sanzidahmed313@gmail.com>
2026-09-21 16:55:56 +0600

    added sort, reverse, copy

---
+++ Python_lists/07_sort_and_reverse.py
@@ -0,0 +1,32 @@
+numbers = [50, 10, 40, 20, 30]
+numbers.sort()
+print(numbers)
+
+
+# Descending order 
+
+numbers = [50, 10, 40, 20, 30]
+numbers.sort(reverse=True)
+print(numbers)
+
+
+
+# String can also be sorted: 
+
+names = ["Zahid", "Rahim", "Ahmed", "Karim"]
+names.sort()
+print(names)
+
+
+
+"""
+1. sort change the original list. 
+"""
+
+
+
+
+
+numbers = [10, 20, 30, 40]
+numbers.reverse()
+print(numbers)
---
+++ Python_lists/Exercise_Python_lists/🧪 Mini Exercise 04 Python lists Answe.py
@@ -0,0 +1,50 @@
+"""
+What will this print? 
+a = [10, 20, 30] 
+b = a 
+
+b[0] = 100 
+
+print(a) 
+print(b)
+
+ans: 
+[100, 20, 30] 
+[100, 20, 30] 
+
+
+
+
+
+
+a = [10, 20, 30] 
+b = a.copy()
+
+b[0] = 100
+
+print(a)
+
+
+ans: a = [10, 20, 30] 
+
+"""
+
+
+# verifu answer: 
+
+a = [10, 20, 30] 
+b = a 
+
+b[0] = 100 
+
+print(a) 
+print(b) 
+
+
+
+a = [10, 20, 30] 
+b = a.copy() 
+
+b[0] = 100 
+
+print(a)
